04b587d388749bade5e1b0e2f3c85bd98b9557c2
Will brown <wtbrown10@gmail.com>
2021-09-02 01:05:59 -0400

    factorial

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -3,14 +3,21 @@ package com.will;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int counter = 0;
+        // write your code here
+        int counter = 10, hold = 1;
+//
+//        while (counter < 15) {
+//            System.out.println("I love Java");
+//            counter ++;
+//        }
+//
+//        System.out.println("Out of while loop!");
+//    }
+        while (counter > 1) {
+            hold *= counter;
+            System.out.println(/*"5*"+ counter + " = " +*/ hold);
+            counter--;
 
-        while (counter < 15) {
-            System.out.println("I love Java");
-            counter ++;
         }
-
-        System.out.println("Out of while loop!");
     }
 }
