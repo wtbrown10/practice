5694511edbf410f6be2873886200e44880e0f716
Will brown <wtbrown10@gmail.com>
2021-09-02 15:53:28 -0400

    do while

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -1,10 +1,12 @@
 package com.will;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int counter = 10, hold = 1;
+//        int counter = 10, hold = 1;
 //
 //        while (counter < 15) {
 //            System.out.println("I love Java");
@@ -13,11 +15,28 @@ public class Main {
 //
 //        System.out.println("Out of while loop!");
 //    }
-        while (counter > 1) {
-            hold *= counter;
-            System.out.println(/*"5*"+ counter + " = " +*/ hold);
-            counter--;
+//        while (counter > 1) {
+//            hold *= counter;
+//            System.out.println(/*"5*"+ counter + " = " +*/ hold);
+//            counter--;
+//
+//        }
 
-        }
+        Scanner sc = new Scanner(System.in);
+
+        String response;
+        do {
+            System.out.println("Enter a whole number between 1 and 50");
+            int number1 = sc.nextInt();
+            System.out.println("Enter another whole number between 1 and 50");
+            int number2 = sc.nextInt();
+            int add = (number2 + number1);
+            System.out.println(number2  + " + "  + number1 + " = " + add);
+            System.out.println("would you like to to perform operation again? Type (Yes) or (No)");
+            response = sc.next();
+        } while (response.equals("Yes"));
+        System.out.println("done!");
+
+
     }
 }
