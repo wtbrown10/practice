7feb9ba599f67ca73c12c612c92c0dd62e032e9f
Will brown <wtbrown10@gmail.com>
2021-09-02 00:03:50 -0400

    first commit

---
+++ src/com/will/Main.java
@@ -0,0 +1,16 @@
+package com.will;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int counter = 0;
+
+        while (counter < 15) {
+            System.out.println("I love Java");
+            counter ++;
+        }
+
+        System.out.println("Out of while loop!");
+    }
+}
