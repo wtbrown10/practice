e183c5b55f138e54596e02034f4b1a6e5cc15dc9
Will brown <wtbrown10@gmail.com>
2021-09-13 16:02:24 -0400

    hashset challenge

---
+++ src/com/will2/HashChallenge.java
@@ -0,0 +1,22 @@
+package com.will2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashChallenge {
+    public static void main(String[] args) {
+
+        Set<String> s1 = new HashSet<String>();
+        s1.addAll(Arrays.asList(new String []{"Ram", "Rack", "Dhoni", "Harshad", "Modi", "Vijay", "Sonia"}));
+        // either or can be used to create hashset object
+        Set<String> s2 = new HashSet<String>(Arrays.asList(new String []{"Ravi", "Faisal", "Prateek", "Abhishek", "Dhoni", "Harshad", "Ram", "Aakash"}));
+
+        Set<String> s3 = new HashSet<String>();
+        s3.addAll(Arrays.asList(new String []{"Modi", "Vijay", "Dhoni", "Faisal", "Prateek", "Harshad", "Ram", "Ravi"}));
+
+        s1.retainAll(s2);
+        s1.retainAll(s3);
+        System.out.println(s1);
+    }
+}
